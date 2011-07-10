75586cc81db46fa7c2d6ad62162f1b4bb7fca5e0
Alec Hipshear <alechipshear@gmail.com>
2011-07-10 17:33:41 -0400

    add relationships for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110709183901) do
+ActiveRecord::Schema.define(:version => 20110710212125) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110709183901) do
     t.datetime "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
